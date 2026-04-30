12fdb25d524780e18b7a3c2b67dd1f92aef05843
Lukemansahid <lukemansahid2020@gmail.com>
2026-04-30 12:10:14 +0000

    I added a method to add two numbers

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 
 public class Main {
-    public static void main(String[] args
-    }
+    public static void main(String[] args){
 
+
+    }
+    void addTwoNumbers(int a, int b){
+        // here is my method to add two numbers
+    }
 }
